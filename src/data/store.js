319f26ba0fab0c961fe6a00354e86c9ae808257c
Lorenzo D'Addazio <ldaddazio11@gmail.com>
2024-06-03 16:12:32 +0200

    first commit

---
+++ src/data/store.js
@@ -0,0 +1,31 @@
+import { reactive } from "vue";
+
+const myData = reactive ({
+
+});
+
+export default myData;
+
+teams:[
+    {
+    urlImg:"../src/assets/team1.png",
+    name:"Cristiano Messi",
+    ruolo:"CEO/Founder"
+},
+{
+    urlImg:"../public/img/team2.png",
+    name:"Sergio Naymer",
+    ruolo:"Programmer"
+},
+{
+    urlImg:"../public/img/team3.png",
+    name:"Antonela Roccuzzo",
+    ruolo:"Progect Manager"
+},
+{
+    urlImg:"../public/img/team4.png",
+    name:"Lionel Ramos",
+    ruolo:"Game Design"
+}
+    
+]
